974b95da688527e2b5ccf45cd2b30a1b12598a11
Aakash Mathur <aakashrajmathur@hotmail.com>
2018-08-09 08:09:04 -0500

    Added CPL18 controller and RaceToPlayoffs view

---
+++ cricpredict/Controllers/CPL18Controller.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace cricpredict.Controllers
+{
+    public class CPL18Controller : Controller
+    {
+        // GET: CPL18
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
